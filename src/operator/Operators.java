b3827603825cbf8a7f7c9dfc7a1b59fb53d119a5
codekul-vaibhav <codekul.vaibhav@gmail.com>
2022-02-16 19:02:18 +0530

    Operators

---
+++ src/operator/Operators.java
@@ -0,0 +1,22 @@
+package operator;
+
+/**
+ *
+ * unary operator -> a++ post increment, ++a pre increment , a-- ,--a
+ * arithmetic operator -> + , - ,/ ,* ,%
+ * relational -> < , > , <=, >=
+ * logical -> & , |
+ * assignment -> = , !=
+ * ternary operator -> : ?
+ * bitwise -> && , ||
+ * shift operator -> <<  >>
+ *
+ */
+public class Operators {
+
+    public static void main(String[] args) {
+        int a = 9;
+        int b =9;
+        System.out.println(a&b);
+    }
+}
