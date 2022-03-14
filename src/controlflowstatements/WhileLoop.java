b340676c5dad01f060ea3f47cd3a720512537b3e
codekul-vaibhav <codekul.vaibhav@gmail.com>
2022-03-14 18:44:54 +0530

    Switch Case

--- src/controlflowstatements/WhileLoop.java
+++ src/controlflowstatements/WhileLoop.java
@@ -1,20 +1,24 @@
 package controlflowstatements;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 public class WhileLoop {
     public static void main(String[] args) {
 
-        int i=0;
-        while (i<=10){
-            System.out.println(i);
-            i++;
-        }
-
-        do {
-            System.out.println(i);
-            i++;
-        }while (i<=10);
+//        int i=0;
+//        while (i<=10){
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//        do {
+//            System.out.println(i);
+//            i++;
+//        }while (i<=10);
+        LocalTime localTime = LocalTime.parse(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
 
     }
 }
