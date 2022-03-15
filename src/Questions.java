a8eb7459fb1de71255085b38dafbef1e0fd2b8d3
codekul-vaibhav <codekul.vaibhav@gmail.com>
2022-03-15 19:07:50 +0530

    Test 15 March

--- src/Questions.java
+++ src/Questions.java
@@ -1,11 +1,11 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.temporal.WeekFields;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Questions {
     public static void main(String[] args) {
-        int[] arr = {103, 23, 45, 545, 55, 101, 102};
+    /*    int[] arr = {103, 23, 45, 545, 55, 101, 102};
 
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
@@ -21,6 +21,11 @@ public class Questions {
         List<Integer> list1 = Stream.of(103, 23, 45, 545, 55, 101, 102).filter(f -> f > 100).sorted().toList();
 
         System.out.println(list1.get(0) + list1.get(1));
+*/
+        LocalDate date =  LocalDate.parse("2022-03-15");
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
+        int weekNumber = date.get(weekFields.weekOfWeekBasedYear());
+        System.out.println(weekNumber);
 
     }
 }
