9d30a60d0ff1b0700704f0eb4cb63f25bd8aea04
codekul-vaibhav <codekul.vaibhav@gmail.com>
2022-04-08 19:03:52 +0530

    queue nad map

---
+++ src/collectionframework/MapDemo.java
@@ -0,0 +1,20 @@
+package collectionframework;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo {
+    public static void main(String[] args) {
+
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"ABC");
+        map.put(2,"PQR");
+        map.put(3,"XYZ");
+
+        map.forEach((k,v)->{
+            System.out.println(k+" "+v);
+        });
+//        System.out.println(map);
+
+    }
+}
