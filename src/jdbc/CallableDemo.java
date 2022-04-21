2d1ab250c22cdcf30992962c2cd97e62ff56c851
codekul-vaibhav <codekul.vaibhav@gmail.com>
2022-04-21 19:05:51 +0530

    Jdbc - CallableDemo, BatchDemo

---
+++ src/jdbc/CallableDemo.java
@@ -0,0 +1,21 @@
+package jdbc;
+
+import java.sql.*;
+
+public class CallableDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");//register driver
+        Connection connection =                            // establishing connection
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/fullstack10feb", "root3", "password");
+
+        CallableStatement callableStatement = connection.prepareCall("call getStudentData()");
+
+        ResultSet resultSet = callableStatement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt(1)
+                    + " " + resultSet.getString(2)+" "+resultSet.getString(3));
+        }
+
+    }
+}
